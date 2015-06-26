9ca2a5d59968e9e0e7db98b42b70c3ae8c19ece0
Evren Kutar <furiston@gmail.com>
2015-06-26 15:22:20 +0300

    get login_crd from dict

--- zaerp/activities/views/core.py
+++ zaerp/activities/views/core.py
@@ -17,22 +17,6 @@ class Dashboard(SimpleView):
 
     def _do(self):
         self._show()
-        # try:
-        #     login_credentials = self.current.request.context.jsonin.login_crd
-        # except KeyError:
-        #     raise HTTPBadRequest("Missing login data")
-        # user = authenticate(login_credentials)
-        # is_login_successful = bool(user)
-        # if is_login_successful:
-        #     self.current.request.context.jsonout = {'success': True}
-        #     self.current.request.session['user'] = user
-        # self.current.task.data['is_login_successful'] = is_login_successful
 
     def _show(self):
-        # if 'user' not in self.current.request.session:
-        #     self.current.request.context['result']['forms'] = get_form(
-        #         'student_login_form')
-        # else:
-        #     self.current.request.context[
-        #         'show_user_message'] = "Zaten giriş yapmış durumdasınız"
         self.current.request.context['result']['dashboard'] = "Dashboard"
