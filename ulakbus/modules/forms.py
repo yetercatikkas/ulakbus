c92c089d7456409eafbeae561d4d26301bb8f97a
Evren Kutar <furiston@gmail.com>
2015-07-30 13:30:13 +0300

    listnode ogesini fieldset olarak forma ekle

--- ulakbus/modules/forms.py
+++ ulakbus/modules/forms.py
@@ -16,15 +16,31 @@ class AngularForm(Form):
             "form": [],
             "model": {}
         }
+        listnode = {"type": "fieldset", "items": []}
+        listnodename = ""
         for itm in self._serialize():
             if isinstance(itm['value'], datetime):
                 itm['value'] = itm['value'].strftime(DATE_FORMAT)
             result["schema"]["properties"][itm['name']] = {'type': itm['type'],
                                                            'title': itm['title']}
             result["model"][itm['name']] = itm['value'] or itm['default']
-            result["form"].append(itm['name'])
+
+            if itm["storage"] == "ListNode":
+                if listnodename == "":
+                    listnodename = itm["section"]
+                    listnode["name"] = listnodename
+                if listnodename == itm["section"]:
+                    listnode["items"].append({"key": itm['name'], "title": itm['title']})
+                else:
+                    result["form"].append(listnode)
+                    listnode = {"type": "fieldset", "items": [], "name": itm["section"]}
+                    listnode["items"].append({"key": itm['name'], "title": itm['title']})
+                    listnodename = itm["section"]
+            else:
+                result["form"].append(itm['name'])
             if itm['required']:
                 result["schema"]["required"].append(itm['name'])
+        result["form"].append(listnode)
         return result
 
 
