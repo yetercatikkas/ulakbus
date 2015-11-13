6ef151c130314a682b70e0fc7c5c65c9664f4b86
yetercatikkas <yetercatikkas@gmail.com>
2015-11-13 09:44:03 +0200

    test_menu added

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -35,6 +35,29 @@ class TestCase(BaseTestCase):
             for value in resp.json[key]:
                 assert set(value.keys()).issubset({'kategori', 'param', 'text', 'url', 'wf', 'model'})
 
+    def test_authorisation_in_menu(self):
+        # setup workflow
+        self.prepare_client('/menu')
+        resp = self.client.post()
+        resp.raw()
+        self.client.user.role_set[0].role.add_permission_by_name('Atama', True)
+        self.client.user.role_set[0].role.add_permission_by_name('Borc', True)
+        sleep(1)
+        self.client.set_path('/logout')
+        self.client.post()
+        self.prepare_client('/menu', login=True)
+        resp = self.client.post()
+        resp.raw()
+        assert set(resp.json.keys()) == set(['other', 'personel', 'ogrenci', 'is_login'])
+        for content in resp.json.keys():
+            try:
+                for i in range(len(resp.json[content])):
+                    assert set(resp.json[content][i].keys()) == set(['url', 'text', 'kategori', 'param'])
+                    assert resp.json[content][i]['url'] in ["crud/Borc", "/yeni_personel", "crud/HizmetBorclanma",
+                                                            "crud/Atama"]
+            except TypeError:
+                assert content
+
     def test_unauthorized_in_menu(self):
         self.client.set_path('/logout')
         self.client.post()
